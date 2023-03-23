c0864ee0f8d84df0707094d14a482dd3148602a5
MinGyu-Kang <kagrin97@gmail.com>
2023-03-23 19:12:50 +0900

    feat : prod일떄 TerserPlugin를 적용해 주석 및 콘솔로그 제거

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,26 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'hidden-source-map',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+          format: {
+            comments: false,
+          },
+        },
+      }),
+    ],
+  },
   module: {
     rules: [
       {
